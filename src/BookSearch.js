37bf2b65170075028b4ad65d2ef5960e577da143
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-11-07 13:52:20 -0600

    feat: Add links to home and search

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
 class BookSearch extends React.Component {
 
@@ -6,23 +8,26 @@ class BookSearch extends React.Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-                <div className="search-books-input-wrapper">
-                    {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                    <Link to="/" className="close-search">Close</Link>
 
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                    */}
-                    <input type="text" placeholder="Search by title or author"/>
+                    <div className="search-books-input-wrapper">
+                        {/*
+                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                        You can find these search terms here:
+                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
+                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                        you don't find a specific author or title. Every search is limited by search terms.
+                        */}
+                        <input type="text" placeholder="Search by title or author"/>
+
+                    </div>
                 </div>
-                </div>
+                
                 <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                    <ol className="books-grid"></ol>
                 </div>
+                
             </div>
         )
     }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -11,44 +11,45 @@ class ListBooks extends Component {
             
             <div className="list-books">
                 <div className="list-books-title">
-                <h1>MyReads</h1>
+                    <h1>MyReads</h1>
                 </div>
                 <div className="list-books-content">
-                <div>
-                    <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                            {books.filter(book => book.shelf==="currentlyReading").map(book => (
-                                <Book book={book} onChangeShelf={onChangeShelf} />
-                            ))}
-                        </ol>
+                    <Link to="/search" >Go to Search</Link>
+                    <div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Currently Reading</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {books.filter(book => book.shelf==="currentlyReading").map(book => (
+                                        <Book book={book} onChangeShelf={onChangeShelf} />
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Want to Read</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {books.filter(book=>book.shelf==="wantToRead").map(book=> (
+                                        <Book book={book} onChangeShelf={onChangeShelf}/>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Read</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {books.filter(book=>book.shelf==="read").map(book=> (
+                                        <Book book={book} onChangeShelf={onChangeShelf}/>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
                     </div>
-                    </div>
-                    <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                            {books.filter(book=>book.shelf==="wantToRead").map(book=> (
-                                <Book book={book} onChangeShelf={onChangeShelf}/>
-                            ))}
-                        </ol>
-                    </div>
-                    </div>
-                    <div className="bookshelf">
-                    <h2 className="bookshelf-title">Read</h2>
-                    <div className="bookshelf-books">
-                        <ol className="books-grid">
-                            {books.filter(book=>book.shelf==="read").map(book=> (
-                                <Book book={book} onChangeShelf={onChangeShelf}/>
-                            ))}
-                        </ol>
-                    </div>
-                    </div>
-                </div>
                 </div>
                 <div className="open-search">
-                <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+                    <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
                 </div>
             </div>
         )
